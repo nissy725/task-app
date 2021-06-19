742ebd96bd74f536a22964d99899d3969c0e9c8c
nissy725 <takanobu_725@yahoo.co.jp>
2021-06-19 17:17:27 +0900

    paramsの登録を実装

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -4,4 +4,19 @@ class CustomersController < ApplicationController
   def new
     @customer = Customer.new
   end
+
+  def create
+    @customer = Customer.new(customer_params)
+    if @customer.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def customer_params
+    params.require(:customer).permit(:name, :product, :count).merge(user_id: current_user.id)
+  end
 end
